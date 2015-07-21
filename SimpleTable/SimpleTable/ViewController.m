4bc77b8e639c8929e25d6ecb99fedd01279faea6
daichao <wuxirendaichao@aliyun.com>
2015-07-22 00:33:26 +0800

    update the simpletableapp

--- SimpleTable/SimpleTable/ViewController.m
+++ SimpleTable/SimpleTable/ViewController.m
@@ -10,8 +10,9 @@
 #import "SimpleTableCell.h"
 @interface ViewController ()
 {
-    NSArray *tableData;
+    NSMutableArray *tableData;
     NSArray *thumbnails;
+    NSArray *prepTime;
 }
 @end
 
@@ -19,9 +20,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    NSString *path=[[NSBundle mainBundle]pathForResource:@"recipes" ofType:@"plist"];
+//    NSDictionary *dict=[[NSDictionary alloc]initWithContentsOfFile:path];
+//    tableData=[dict objectForKey:@"RecipeName"];
+//    thumbnails=[dict objectForKey:@"Thumbnail"];
+//    prepTime=[dict objectForKey:@"PrepTime"];
     // Do any additional setup after loading the view, typically from a nib.
-    tableData=[NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini",nil];
+    tableData=[NSMutableArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini",nil];
     thumbnails=[NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
+    prepTime = [NSArray arrayWithObjects:@"30 min", @"30 min", @"20 min", @"30 min", @"10 min", @"1 hour", @"45 min", @"5 min", @"30 min", @"8 min", @"20 min", @"20 min", @"5 min", @"1.5 hour", @"4 hours", @"10 min", nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -41,9 +48,11 @@
         NSArray *nib=[[NSBundle mainBundle]loadNibNamed:@"SimpleTableCell" owner:self options:nil];
         cell=[nib objectAtIndex:0];
     }
+    
+    
     cell.nameLabel.text=[tableData objectAtIndex:indexPath.row];
     cell.thumbnailImageView.image=[UIImage imageNamed:[thumbnails objectAtIndex:indexPath.row]];
-//    cell.prepTimeLabel.text=[pre];
+    cell.prepTimeLabel.text=[prepTime objectAtIndex:indexPath.row];
     return cell;
 }
 
@@ -51,5 +60,23 @@
     return 78;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UIAlertView *messageAlert=[[UIAlertView alloc]initWithTitle:@"Row selected" message:[tableData objectAtIndex:indexPath.row] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [messageAlert show];
+    
+    UITableViewCell *cell=[tableView cellForRowAtIndexPath:indexPath];
+    cell.accessoryType=UITableViewCellAccessoryCheckmark;
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+   
+
+}
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableData removeObjectAtIndex:indexPath.row];
+    [tableView reloadData];
+}
+//
+//-(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath{
+//     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+//}
 
 @end
