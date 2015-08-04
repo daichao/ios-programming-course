21896aeaefddb4305e32d045a830dc100d3563cc
daichao <wuxirendaichao@aliyun.com>
2015-08-04 22:17:28 +0800

    add the staticDemo

---
+++ StaticTableDemo/StaticTableDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StaticTableDemo
+//
+//  Created by daichao on 15/8/4.
+//  Copyright (c) 2015年 daichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StaticTableDemo/StaticTableDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticTableDemo
+//
+//  Created by daichao on 15/8/4.
+//  Copyright (c) 2015年 daichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
