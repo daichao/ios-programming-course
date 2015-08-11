d4a67aac0e2e92890bb68772c8cec609ad8155db
bokeadmin <bokeadmin@bokeMac.local>
2015-08-11 16:02:09 +0800

    add the Cloud Backend use Parse

---
+++ RecipeBookParseEmpty/RecipeBook/RecipeDetailViewController.h
@@ -0,0 +1,21 @@
+//
+//  RecipeDetailViewController.h
+//  RecipeBook
+//
+//  Created by Simon Ng on 17/6/12.
+//  Copyright (c) 2012 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Recipe.h"
+//#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+@interface RecipeDetailViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet PFImageView *recipePhoto;
+@property (weak, nonatomic) IBOutlet UILabel *prepTimeLabel;
+@property (weak, nonatomic) IBOutlet UITextView *ingredientTextView;
+
+@property (nonatomic, strong) Recipe *recipe;
+
+@end
